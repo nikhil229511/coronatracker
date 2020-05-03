27fb602ecf0bcdb78f607410c4a8529989e0aabe
nikhilpatelcaptech <nikhilpatelcaptech@github.com>
2020-05-03 19:50:06 +0530

    Updates

---
+++ precache-manifest.9fde33a505075afcafcf92f369c32c4c.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "222d91bbfa4e03daec2822ec5b235beb",
+    "url": "/coronatracker/index.html"
+  },
+  {
+    "revision": "449b26980bc600950d9d",
+    "url": "/coronatracker/static/css/main.799cd3e8.chunk.css"
+  },
+  {
+    "revision": "85303c22f3d59126c489",
+    "url": "/coronatracker/static/js/2.5be04345.chunk.js"
+  },
+  {
+    "revision": "176852ae27885dfd7559e0fb4b332d24",
+    "url": "/coronatracker/static/js/2.5be04345.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "449b26980bc600950d9d",
+    "url": "/coronatracker/static/js/main.fcf91733.chunk.js"
+  },
+  {
+    "revision": "c4e9d4ac3550c033ed01",
+    "url": "/coronatracker/static/js/runtime-main.51613ec7.js"
+  },
+  {
+    "revision": "d7265326c3a1da5ec94c50746235f734",
+    "url": "/coronatracker/static/media/image.d7265326.png"
+  }
+]);
